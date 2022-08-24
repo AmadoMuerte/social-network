b47b721aae43e64a8a209f0ff60e695987aff068
Amado <kisik854@gmail.com>
2022-08-25 00:03:11 +1000

    add props description for post

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -5,7 +5,7 @@ import ButtonLike from "../../buttons/ButtonLike/ButtonLike";
 
 function Post(props) {
     return (
-        <div className={classes.post}>s
+        <div className={classes.post}>
             <div className={classes.post__inner}>
                 <div className={classes.post__avatar}>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Felis_silvestris_silvestris.jpg" alt="cat"/>
